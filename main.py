25f839f83be058cdebeac433a5655ee8ffc73e6f
JuliaaNaa2107 <julia210797@web.de>
2025-06-18 23:47:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telegram_bot import TelegramBot
 
 # Trage hier deinen echten Bot-Token ein
-bot = TelegramBot("DEIN_BOT_TOKEN", "7737959011")
+bot = TelegramBot("8061147662:AAHGUYqhOYNwJALrEa-kIxnq6WlzXRR05eM", "7737959011")
 
 bot.send_message("✅ Erfolgreich verbunden! Dein Forex-Bot funktioniert.")
